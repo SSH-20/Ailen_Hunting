9bac6efa23047a06e44085aba11d8fc9df237961
Jaeyoung-chutoer <chutoer@gmail.com>
2023-07-06 18:55:10 +0900

    메인메뉴

---
+++ Assets/Scripts/ClickPlayButton.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ClickPlayButton : MonoBehaviour,IPointerDownHandler,IPointerUpHandler
+{
+    [SerializeField] private Image _img;
+    [SerializeField] private Sprite _default, _pressed;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        _img.sprite = _pressed;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        _img.sprite = _default;
+    }
+
+    public void IWasClicked()
+    {
+        Debug.Log("Clicked");
+    }
+}
